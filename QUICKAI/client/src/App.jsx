f9a2cb8463d003f5f403dd7621565a759d304a94
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-08-24 15:56:08 +0530

    file updated

--- QUICKAI/client/src/App.jsx
+++ QUICKAI/client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
+import Home from './pages/Home.jsx'
 import Layout from './pages/Layout'
 import WriteArticle from './pages/WriteArticle'
 import Dashboard from './pages/Dashboard'
