c8abe34ce9d6d56c557ddd401859e9bf1640cc8a
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-08-24 15:48:01 +0530

    file route added

--- QUICKAI/client/src/App.jsx
+++ QUICKAI/client/src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Layout from './pages/Layout'
 import WriteArticle from './pages/WriteArticle'
+import Dashboard from './pages/Dashboard'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />}/>
           <Route path='/ai' element={<Layout />}>
+            <Route index element={<Dashboard/>}/>
             <Route path='write-article' element={<WriteArticle />} />
             <Route path='blog-titles' element={<BlogTitles />} />
           </Route>
